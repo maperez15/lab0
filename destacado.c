5bee444757a57ebcdf41e1811dc61bdfebbf81bf
maperez15 <maperez15@uc.cl>
2019-08-11 18:49:54 -0400

    Destacado final

--- destacado.c
+++ destacado.c
@@ -1,39 +1,55 @@
 #include<stdio.h>
 #include<unistd.h>
 
+
 int main()
 {
     printf("\n\n\t\tCalcularemos tu promedio\n\n\n");
 
-    int n, i1, k,i2;
-    float sum = 0, x, average=0, average2=0;
+    int n, i1, k,i2, t, i3, n2;
+    float sum = 0, x, average=0, p=0, sum2=0;
+
 
     printf("Cuantos ramos tienes este semestre: ");
     scanf("%d", &n);
 
-    printf("\n\n\nEnter %d elements\n\n", n);
-    for(i1 = 0; i1 < n; i1++)
-    {
-      printf("\nCuantas notas tiene tu ramo %d:\t",i1+1);
 
+
+    for(i1 = 0; i1 < n; i1++) //n es la cantidad de ramos
+    {
+      printf("\nCuantos tipos de evaluación tiene el ramo %d:\t",i1+1);
       scanf("%d",&k);
 
-      for(i2=0; i2<k; i2++)
-      {
-        scanf("%f", &x);
-        sum += x;
 
+      for(i2=0; i2<k; i2++) //Tipos de evaluación de el ramo
+      {
+      printf("\n Que ponderacion tiene la evaluacion tipo %d: ", i2+1);
+      scanf("%f",&p);
+      printf("\n Cuantas evaluaciones hay de este tipo?");
+      scanf("%d",&n2);
+      printf("Escribe las %d notas",n2);
+
+      for (i3=0; i3<n2; i3++)
+        { scanf("%f", &x);
+          sum+=x;
+        }
+      //el promedio de notas del tipo de evaluacion se tiene con sum y dividiendo por n2
+
+      average+=(p/100)*(sum/n2);
+      sum=0;
       }
 
-    printf("tu promedio en ramo %d es %f",i1+1, sum/k );
-    printf("%f\n\n",k);
+      printf("tu promedio en ramo %d es %f",i1+1, average);
 
-    average+=(sum/k);
+      sum2+=average;
+      average=0;
+
     }
-    average2+=(average);
 
-    printf("\n\n\nTo promedio final es =  %f", (average2/n));
+
+    printf("\n\n\nTo promedio final es =  %f", (sum2/n));
     sleep(1000);
+    getchar();
 
     return 0;
 }
